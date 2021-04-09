e90b404a85b15b79cdc16e2b390454d9022f55c9
Andrei <andreikagit@users.noreply.github.com>
2021-04-09 21:34:22 +0300

    update kicad gerber processing scripts

--- copy_all_modules_from_Kicad.sh
+++ copy_all_modules_from_Kicad.sh
@@ -2,6 +2,6 @@
 
 cd hellen-one
 
-python2.7 ./bin/copy_from_Kicad.py "modules" "kicad/modules" "wbo" "0.1"
+python2.7 ./bin/copy_from_Kicad.py "modules" "kicad/modules" "gerber" "wbo" "0.1"
 
 echo "Done!"
