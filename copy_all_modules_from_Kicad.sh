310b69a14c900241106e5bcc9db878439182342d
Andrei <andreikagit@users.noreply.github.com>
2021-05-13 21:12:01 +0300

    add vr-discrete to copy_all_modules_from_Kicad.sh

--- copy_all_modules_from_Kicad.sh
+++ copy_all_modules_from_Kicad.sh
@@ -3,5 +3,6 @@
 cd hellen-one
 
 python2.7 ./bin/copy_from_Kicad.py "modules" "kicad/modules" "gerber" "wbo" "0.1"
+python2.7 ./bin/copy_from_Kicad.py "modules" "kicad/modules" "gerber" "vr-discrete" "0.1"
 
 echo "Done!"
